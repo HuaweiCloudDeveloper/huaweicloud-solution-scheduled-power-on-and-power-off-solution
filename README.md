6eff5a32f49ea8f45331e51f1bb414cdf852549f
huaweicloud <hwcloudsolution@163.com>
2023-06-02 15:32:46 +0800

    Scheduled Startup/Shutdown
    Match-id-bc15407dfec034e811d53d0f9f04b667d0b96da6

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# huaweicloud-solution-scheduled-power-on-and-power-off-solution-en
+[TOC]
 
+**Solution Overview**
+===============
+This solution uses a FunctionGraph timer trigger to periodically start and stop Elastic Cloud Servers (ECSs). You can stop unused ECSs, and later start them when needed to reduce operations costs.
+
+For more details about this solution, visit: https://www.huaweicloud.com/intl/en-us/solution/implementations/secsss.html
+
+
+**Architecture**
+---------------
+![Architecture](./document/Scheduled-ECS-Startup&Shutdown.png)
+
+**Architecture Description**
+---------------
+This solution will:
+
+1. Create two functions in FunctionGraph to call the ECS APIs for periodically starting and stopping ECSs.
+
+2. Create a timer to periodically invoke the function code.
+
+3. Create an agency in Identity and Access Management (IAM) to delegate FunctionGraph to access ECS in the relevant region.
+
+**File Structure**
+---------------
+
+``` lua
+huaweicloud-solution-scheduled-power-on-and-power-off-solution
+├── scheduled-power-on-and-power-off-solution.tf.json -- Resource orchestration template
+├── functiongraph
+	├── batch-start-ecs-at-scheduled-time.py  -- Function file (startup)
+	├── batch-stop-ecs-at-scheduled-time.py  -- Function file (shutdown)
+```
+**Getting Started**
+---------------
+
+1. Go to the [FunctionGraph console](https://console-intl.huaweicloud.com/functiongraph/?agencyId=f9a6de0674e84d4686ff806fc4eb78e8&region=ap-southeast-3&locale=en-us#/serverless/functionList), and view the created functions in the function list.
+
+	Figure1 Created functions
+
+	![Created functions](./document/readme-image-001.png)
+
+2. On the details page of each function, view the created trigger.
+
+	Figure2 Startup trigger
+
+	![Startup trigger](./document/readme-image-002.png)
+
+	Figure3 Shutdown trigger
+
+	![Shutdown trigger](./document/readme-image-003.png)
+
+3. On the Monitoring tab, check that each trigger started or stopped the specified ECSs at the specified time.
+
+	Figure4 Request logs
+
+	![Request logs](./document/readme-image-004.png)
---
+++ functiongraph/batch-stop-ecs-at-scheduled-time.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+
+from huaweicloudsdkcore.auth.credentials import BasicCredentials
+from huaweicloudsdkecs.v2.region.ecs_region import EcsRegion
+from huaweicloudsdkcore.exceptions import exceptions
+from huaweicloudsdkecs.v2 import *
+
+import traceback
+
+# FunctionGraph entry point
+def handler(event, context):
+    log = context.getLogger()
+    result = check_configuration(context)
+    if result is not None:
+        return result
+    
+    processor = Processor(context)
+   
+    try:
+        response = processor.stop_servers()
+        return f"'{response}'"
+    except:
+        log.error("failed to process, "
+                  f"exception:{traceback.format_exc()}")
+                  
+# Verify that environment variables have been configured.
+def check_configuration(context):
+    ak = context.getAccessKey().strip()
+    sk = context.getSecretKey().strip()
+    if not ak or not sk:
+        ak = context.getUserData('ak', '').strip()
+        sk = context.getUserData('sk', '').strip()
+        if not ak or not sk:
+            return 'ak or sk is empty'
+
+
+class Processor:
+    def __init__(self,context=None):
+        self.log = context.getLogger()
+        self.os_client = os_client(context)
+        self.region = context.getUserData('region').strip()
+        self.ids = context.getUserData('ids').strip().split()
+
+    def build_servers_options(self):
+        listServersOsstart = []
+
+        for os_id in self.ids:
+            listServersOsstart.append(
+                ServerId(
+                    id = os_id
+                )
+            )
+        
+        return listServersOsstart
+
+    def stop_servers(self):
+        try:
+            request = BatchStopServersRequest()
+
+            osstopbody = BatchStopServersOption(
+                servers = self.build_servers_options()
+            )
+            request.body = BatchStopServersRequestBody(
+                os_stop = osstopbody    
+            )
+
+            return self.os_client.batch_stop_servers(request)
+        except exceptions.ClientRequestException as e:
+            print(e.status_code)
+            print(e.request_id)
+            print(e.error_code)
+            print(e.error_msg)
+            self.log.error(f"failed to request batch stop servers"
+                           f"status_code:{e.status_code}, "
+                           f"request_id:{e.request_id}, "
+                           f"error_code:{e.error_code}. "
+                           f"error_msg:{e.error_msg}")
+
+
+def os_client(context):
+    ak = context.getAccessKey()
+    sk = context.getSecretKey()
+    credentials = BasicCredentials(ak, sk)
+
+    return EcsClient.new_builder() \
+        .with_credentials(credentials) \
+        .with_region(EcsRegion.value_of(context.getUserData('region'))) \
+        .build()
